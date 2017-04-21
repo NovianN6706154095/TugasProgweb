f70f88cac409e3da42caffce1a80711e53b11ce3
Novian Nuraldi <novian.nuraldi97@gmail.com>
2017-04-21 17:35:07 +0700

    Update home.php

--- home.php
+++ home.php
@@ -5,7 +5,7 @@ if(isset($_SESSION['username'])){
 	//koneksi terpusat
 	include "conn.php";
 	$username=$_SESSION['username'];
-?>
+?> //ini tampilan koneksi
 
     <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
     <html xmlns="http://www.w3.org/1999/xhtml">
