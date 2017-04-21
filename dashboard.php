44539d50edfd2a18857855c648a5c6882e4bc6a8
Deden <dedennursyadid@gmail.com>
2017-04-21 16:51:29 +0700

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,44 @@
+<?php 
+if($_SESSION['domain']=='guru'){
+	$id_guru=$_SESSION['id_guru'];
+	$username=ucwords($_SESSION['username']);
+	
+	$data=mysql_fetch_array(mysql_query("select * from data_guru where id_guru='$id_guru'"));
+
+	$kelamin=$data['kelamin'];
+	
+	if($kelamin=='laki-laki'){
+		$sapaan='Pak ';
+	}else{
+		$sapaan='Ibu ';
+	}
+	
+	$pengguna=$sapaan.$username;
+}else{
+	$pengguna=ucwords($_SESSION['username']);
+}
+?>
+
+<div id="page-heading">
+	<center>
+    <h1>---SELAMAT DATANG---</h1>
+    
+	</center>
+</div>
+<!-- end page-heading -->
+
+<table border="0" width="100%" cellpadding="0" cellspacing="0" id="content-table">
+<tr>
+    <th rowspan="3" class="sized"><img src="images/shared/side_shadowleft.jpg" width="20" height="300" alt="" /></th>
+    <th class="topleft"></th>
+    <td id="tbl-border-top">&nbsp;</td>
+    <th class="topright"></th>
+    <th rowspan="3" class="sized"><img src="images/shared/side_shadowright.jpg" width="20" height="300" alt="" /></th>
+</tr>
+
+<tr>
+    <th class="sized bottomleft"></th>
+    <td id="tbl-border-bottom">&nbsp;</td>
+    <th class="sized bottomright"></th>
+</tr>
+</table>
